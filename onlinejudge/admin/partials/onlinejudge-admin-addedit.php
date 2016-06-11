a5cf6ab8d717c12938153cf97a1629020f62a55d
Miguel Revilla <yo@miguelrevilla.com>
2016-06-11 14:34:39 +0200

    Added problemtype select

--- onlinejudge/admin/partials/onlinejudge-admin-addedit.php
+++ onlinejudge/admin/partials/onlinejudge-admin-addedit.php
@@ -90,7 +90,15 @@ class OnlineJudge_AdminAddEdit {
 			case 'datetime':
 				return '<input name="' . $field['dbname'] . '"'.($this->action=='edit'?' value="'.$value.'"':'').' class="datefield" />' ;
 			case 'problemtype':
-				return 'Problem Type';
+				$probtype_array = array() ;
+				$probtype_array = $wpdb->get_results("SELECT id,name FROM ".$wpdb->prefix."oj_problemtypes ORDER BY id ASC",ARRAY_A) ;
+
+				$returnstr = '<select name="problemtype">' ;
+				foreach($probtype_array as $problemtype) {
+					$returnstr .= '<option value="'.$problemtype['id'].'"'.($value==$problemtype['id']?' selected':'').'>'.$problemtype['name'].'</option>' ;
+				}
+				$returnstr .= '</select>' ;
+				return $returnstr;
 			case 'categories':
 				$cat_array = array() ;
 				$cat_array = $wpdb->get_results("SELECT id,name FROM ".$wpdb->prefix."oj_categories ORDER BY id ASC",ARRAY_A) ;
