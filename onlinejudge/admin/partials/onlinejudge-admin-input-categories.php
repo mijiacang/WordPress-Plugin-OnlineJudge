c5659e8e9325ce53b4f93531e2204b9148dda39e
Miguel Revilla <yo@miguelrevilla.com>
2016-06-30 13:17:50 +0200

    Added chosen dropdown jquery plugin

--- onlinejudge/admin/partials/onlinejudge-admin-input-categories.php
+++ onlinejudge/admin/partials/onlinejudge-admin-input-categories.php
@@ -1,7 +1,11 @@
 <?php
 class OnlineJudge_AdminInputCategories {
 
+	private $tmp_cats ;
+
 	public function __construct() {
+
+		$this->tmp_cats = array() ;
 
 	}
 
@@ -10,15 +14,26 @@ class OnlineJudge_AdminInputCategories {
 		global $wpdb ;
 
 		$cat_array = array() ;
-		$cat_array = $wpdb->get_results("SELECT id,name FROM ".$wpdb->prefix."oj_categories ORDER BY id ASC",ARRAY_A) ;
+		$cat_array = $wpdb->get_results("SELECT id,name,parent FROM ".$wpdb->prefix."oj_categories ORDER BY id ASC",ARRAY_A) ;
+
+		$this->sortCats($cat_array) ;
 
 		$returnstr = '<select name="parent">' ;
-		foreach($cat_array as $cat) {
-			$returnstr .= '<option value="'.$cat['id'].'"'.($value==$cat['id']?' selected':'').'>'.$cat['name'].'</option>' ;
+		foreach($this->tmp_cats as $cat) {
+			$returnstr .= '<option value="'.$cat['id'].'"'.($value==$cat['id']?' selected':'').' style="padding-left:'.($cat['level']*15).'px !important;">'.$cat['name'].'</option>' ;
 		}
 		$returnstr .= '</select>' ;
 		return $returnstr;
 	}
 
+	function sortCats($cats,$level=0,$parent=null) {
+		foreach($cats as $cat) {
+			if($cat['parent']==$parent) {
+				$cat['level'] = $level ;
+				array_push($this->tmp_cats,$cat) ;
+				$this->sortCats($cats,$level+1,$cat['id']) ;
+			}
+		}
+	}
 }
 ?>
