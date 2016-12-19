7cdfb3064ac58239a5be78802bbb1b9c6d3cdad9
Miguel Revilla <yo@miguelrevilla.com>
2016-12-19 19:52:09 +0100

    Submissions API mostly done

--- onlinejudge/public/partials/api-submissions.php
+++ onlinejudge/public/partials/api-submissions.php
@@ -1,23 +1,28 @@
 <?php
 
-echo "You asked for submissions, starting with ".$wp_query->query_vars['page'] ;
+global $wpdb ;
+
+$conditions = array() ;
+$count = null ;
+
+$conditions[] = "id >= ".$wp_query->query_vars['page'] ;
 
 if(isset($wp_query->query_vars['problemid']) && $wp_query->query_vars['problemid'] > 0) {
-	echo ", for problem ".$wp_query->query_vars['problemid'] ;
-} else {
-	echo ", for all problems" ;
+	$conditions[] = " AND problem=".$wp_query->query_vars['problemid'] ;
 }
 
 if(isset($wp_query->query_vars['userid']) && $wp_query->query_vars['userid'] > 0) {
-	echo ", for user ".$wp_query->query_vars['userid'] ;
-} else {
-	echo ", for all users" ;
+	$conditions[] = " AND user=".$wp_query->query_vars['userid'] ;
 }
 
 if(isset($wp_query->query_vars['listlength']) && $wp_query->query_vars['listlength'] > 0) {
-	echo ", with a length of ".$wp_query->query_vars['listlength'] ;
-} else {
-	echo ", with full length" ;
+	$count = $wp_query->query_vars['listlength'] ;
 }
 
+$querystr = 'SELECT id,user,problem,language,created FROM '.$wpdb->prefix.'oj_submissions WHERE ' ;
+foreach($conditions as $cond) $querystr .= $cond ;
+$querystr .= " ORDER BY id" ;
+if($count != null) $querystr .= " LIMIT $count" ;
+
+echo json_encode($wpdb->get_results($querystr),JSON_NUMERIC_CHECK) ;
 ?>
