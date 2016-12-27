ec00df9b206f103e04a16211a838e15dc8607bb4
Miguel Revilla <yo@miguelrevilla.com>
2016-12-27 17:33:24 +0100

    The begginings of the submissions table

--- onlinejudge/public/js/submissions.js
+++ onlinejudge/public/js/submissions.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function($) {
 
+function fill_table(data) {
+	$('#submissions_body').html('') ;
+	var $bgcolor = 0 ;
+	$.each(data,function(key,val){
+		$('#submissions_body').append('<tr class="color'+$bgcolor+'"><td>'+val.id+'</td><td>'+val.problem+'</td><td>'+val.user+'</td><td>N/A</td><td>'+val.language+'</td><td>N/A</td><td>'+val.created+'</td></tr>') ;
+		$bgcolor = ($bgcolor==0?1:0) ;
+	});
+}
+
+function call_json() {
+	$.getJSON('/api/submissions/0/0/0/0/',fill_table);
+}
+
+setInterval(call_json(),1000) ;
+
 
 }) ;
--- onlinejudge/public/templates/archive-submissions.php
+++ onlinejudge/public/templates/archive-submissions.php
@@ -5,8 +5,18 @@ add_filter('wpseo_title',function($title){ return "Submissions - ".get_bloginfo(
 <?php get_header(); ?>
 
 <header class="entry-header">
-	<h1 class="entry-title">Submissions</h1>
+<h1 class="entry-title">Submissions</h1>
 </header>
+
+<table id="submissions">
+<thead id="submissions_header">
+<th>ID</th><th>Problem</th><th>User</th><th>Verdict</th><th>Language</th><th>Runtime</th><th>Submission time</th>
+</thead>
+<tbody id="submissions_body">
+</tbody>
+</table>
+
+<script type="text/javascript" src="/wp-content/plugins/onlinejudge/public/js/submissions.js"></script>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
