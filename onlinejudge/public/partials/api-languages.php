42ac6ec9219ef3828f0826b4ecd21103f1b058c0
Miguel Revilla <yo@miguelrevilla.com>
2017-01-01 12:00:35 +0100

    Added single language selection

--- onlinejudge/public/partials/api-languages.php
+++ onlinejudge/public/partials/api-languages.php
@@ -2,9 +2,12 @@
 
 global $wpdb ;
 
-$result = array() ;
-$result = $wpdb->get_results('SELECT id,shortname FROM '.$wpdb->prefix.'oj_languages ORDER BY id') ;
+$language = $wp_query->query_vars['page'] ;
 
-echo json_encode($result,JSON_NUMERIC_CHECK) ;
+$result = array() ;
+$result = $wpdb->get_results('SELECT id,shortname FROM '.$wpdb->prefix.'oj_languages '.
+				($language>0?'WHERE id='.$language.' ':'').'ORDER BY id',ARRAY_A) ;
+
+echo json_encode(($language>0?$result[0]:$result),JSON_NUMERIC_CHECK) ;
 
 ?>
